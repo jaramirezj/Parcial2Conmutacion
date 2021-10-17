f7d919bad23fc5197b0ea78631ecd2c018f62ca1
= <jaramirezj@eafit.edu.co>
2021-10-16 19:36:17 -0500

    Añadido codigo de tempertaura y humedad, Conexion a wifi
    Se añade el codigo para tomar el promedio de la temperatura y humedad en los ultimos 30 segundos, ademas de estrablecer una conexión wifi.

--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,53 @@
+#include <ESP8266WiFi.h>
 #include <DHT.h>
 
+const char* ssid = "";
+const char* password = "";
+const char* host = "192.168.1.29";
+const int port =80;
+const int watchdog = 5000;
 #define DHTTYPE DHT11 
 #define dht_dpin 0
 DHT dht(dht_dpin, DHTTYPE);
+
+unsigned long tInicial;
+unsigned long tActual;
+float tempT = 0;
+float tempH = 0;
+float temperatura;
+float humedad;
+
 void setup() {
-  dht.begin();
   Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("/n Conectado");
+  dht.begin();
+}
+
+void enviarDatos(){
+  Serial.print("T:");
+  Serial.print(temperatura);
+   Serial.print(" H:");
+  Serial.println(humedad);
+  tempT = 0;
+  tempH = 0;
 }
 
 void loop() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  Serial.print("T:");
-  Serial.print(t);
-   Serial.print(" H:");
-  Serial.println(h);
-  delay(1000);
+  tInicial = millis();
+  tActual = millis();
+  while(tActual - tInicial <= 30000){
+    
+    tempT += dht.readTemperature();
+    tempH += dht.readHumidity();
+    delay(1000);
+    tActual = millis();
+  }
+  temperatura = tempT / 30;
+  humedad = tempH / 30;
+  enviarDatos();
 }
